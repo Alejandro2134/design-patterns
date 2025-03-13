0cbf87e76f59f367d5a090d71ce0f16700e6c04d
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-03-12 23:43:56 -0500

    fix: main

--- 4-exercise/src/main.ts
+++ 4-exercise/src/main.ts
@@ -4,29 +4,25 @@ import { CompleteCommand } from "./commands/CompleteCommand";
 import { Task } from "./tasks/Task";
 import { TaskManager } from "./tasks/TaskManager";
 
-const main = () => {
-  const taskManager = new TaskManager();
-  const commandInvoker = new CommandInvoker();
+const taskManager = new TaskManager();
+const commandInvoker = new CommandInvoker();
 
-  const task = new Task("Make dinner");
+const task = new Task("Make dinner");
 
-  console.log(`Task ${task.getName()} has state: ${task.getState()}`);
+console.log(`Task ${task.getName()} has state: ${task.getState()}`);
 
-  taskManager.saveTaskState(task);
-  commandInvoker.setCommand(new CompleteCommand(task));
-  commandInvoker.executeCommand();
+taskManager.saveTaskState(task);
+commandInvoker.setCommand(new CompleteCommand(task));
+commandInvoker.executeCommand();
 
-  console.log(`Task ${task.getName()} has state: ${task.getState()}`);
+console.log(`Task ${task.getName()} has state: ${task.getState()}`);
 
-  taskManager.saveTaskState(task);
-  commandInvoker.setCommand(new DeleteCommand(task));
-  commandInvoker.executeCommand();
+taskManager.saveTaskState(task);
+commandInvoker.setCommand(new DeleteCommand(task));
+commandInvoker.executeCommand();
 
-  console.log(`Task ${task.getName()} has state: ${task.getState()}`);
+console.log(`Task ${task.getName()} has state: ${task.getState()}`);
 
-  taskManager.undoTask(task);
+taskManager.undoTask(task);
 
-  console.log(`Task ${task.getName()} has state: ${task.getState()}`);
-};
-
-main();
+console.log(`Task ${task.getName()} has state: ${task.getState()}`);
